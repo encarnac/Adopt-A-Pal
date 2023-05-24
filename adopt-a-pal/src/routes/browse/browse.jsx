7090082cf711921ca4bc7e1f7d35e11763cfab72
Colene Encarnado <encarnac@oregonstate.edu>
2023-05-24 14:53:16 -0700

    Created new route to Browse screen

---
+++ adopt-a-pal/src/routes/browse/browse.jsx
@@ -0,0 +1,13 @@
+import { React, useState } from "react";
+import NavBar from "../../components/navbar";
+
+function Browse(props) {
+
+  return (
+    <>
+      <NavBar currentPage={ "browse" } />
+    </>
+  );
+}
+
+export default Browse;
