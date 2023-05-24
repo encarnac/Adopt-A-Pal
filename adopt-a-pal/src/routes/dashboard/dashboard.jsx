61a6f9f9013955f8db99dfc21ed455d5744786c6
Colene Encarnado <encarnac@oregonstate.edu>
2023-05-24 14:55:01 -0700

    Created modular, reusable navbar depending on page

--- adopt-a-pal/src/routes/dashboard/dashboard.jsx
+++ adopt-a-pal/src/routes/dashboard/dashboard.jsx
@@ -2,7 +2,7 @@ import { React, useState, useEffect, useCallback } from "react";
 import UseUserPals from '../../modules/UseUserPals';
 import UseGetPalById from "../../modules/UseGetPalById";
 import jwtDecode from 'jwt-decode'
-import NavBar from "./navbar";
+import NavBar from "../../components/navbar";
 
 async function fetchPalData(pals) {
     const palDataList = await Promise.all(
@@ -15,15 +15,6 @@ async function fetchPalData(pals) {
   }
 
 function Dashboard(props) {
-  const [loginModal, setLoginModal] = useState(false);
-  const handleLoginModal = (e) => {
-    setLoginModal(e);
-  };
-
-  const [signupModal, setSignupModal] = useState(false);
-  const handleSignupModal = (e) => {
-    setSignupModal(e);
-  };
 
   const token = localStorage.getItem('token');
   const decoded = jwtDecode(token);
@@ -50,7 +41,7 @@ function Dashboard(props) {
   
   return (
     <>
-      <NavBar {...{ handleLoginModal, handleSignupModal }} />
+      <NavBar currentPage={"dashboard"} />
       <div>
         <h1>User Pals</h1>
         <div> ---- </div>
