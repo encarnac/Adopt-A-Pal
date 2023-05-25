2a363a663758b80d24dca72f9e266ea3460ecbba
Colene Encarnado <encarnac@oregonstate.edu>
2023-05-24 22:42:07 -0700

    Created static Browse page with card carousel

---
+++ adopt-a-pal/src/components/CarouselCard.jsx
@@ -0,0 +1,84 @@
+import { React, useState } from "react";
+import { HiHeart, HiOutlineHeart } from "react-icons/hi";
+
+function CarouselCard({ animal }) {
+  const [activeButton, setActiveButton] = useState(true);
+  const handleClick = () => {
+    setActiveButton(!activeButton);
+  }
+  return (
+    <>
+      {/* TO DO: Use the class "z-50 bg-black bg-opacity-30 backdrop-blur-sm" to toggle background for activate state */}
+      <div className="w-full gap-4 flex-wrap flex justify-center items-center">
+        {/* <!-- CARD --> */}
+        <div className="flex w-[50rem] h-[400px] p-0 bg-white rounded-[35px] shadow-lg hover:shadow-2xl">
+          {/* !------ LEFT COLUMN FOR PET IMAGE -----> */}
+          <div className="flex flex-col basis-1/2 justify-center p-0 m-0">
+            <img
+              className="object-cover h-full rounded-[35px]"
+              src={animal.avatars[0]}
+              alt=""
+            />
+          </div>
+
+          {/* <!------RIGHT COLUMN FOR PET INFO ------> */}
+          <div className="flex flex-col relative basis-1/2 p-8 space-y-2 text-start">
+            {/* <!-- 1st ROW - NAME --> */}
+            <h2 className="font-bold text-xl text-black" t>
+              {animal.name}
+            </h2>
+
+            {/* <!-- 2nd ROW - POST DATE --> */}
+            <p className="text-[14px] text-light-grey">
+              Post date: {animal.added.slice(0, 10)}
+            </p>
+
+            {/* <!-- 3rd ROW - AVAILABILITY, SPECIES, BREED --> */}
+            <div className="flex flex-row flex-wrap space-x-2">
+              <p className="badge badge-success badge-lg ">
+                {animal.availability}
+              </p>
+              <p className="badge badge-info badge-lg ">{animal.species}</p>
+              <p className="badge badge-warning badge-lg ">{animal.breed}</p>
+            </div>
+
+            {/* <!-- 4th ROW - DISPOSITIONS LIST --> */}
+            <ul className="text-[15px] text-grey">
+              {animal.dispositions?.map((description, i) => (
+                <li>{description}</li>
+              ))}
+              {/* <li>Good with other animals</li>
+              <li>Good with children</li>
+              <li>Animal must be leashed at all times</li> */}
+            </ul>
+
+            {/* <!-- 5th ROW - CALL TO ACTION BUTTON --> */}
+            <div className="absolute bottom-4 right-4">
+              {/* Opt 1: Like Button for "browse" cardType */}
+              {activeButton ? (
+                <button
+                  onClick={() => handleClick()}
+                  className="btn btn-primary btn-circle shadow-md hover:shadow-lg"
+                >
+                  {" "}
+                  <HiOutlineHeart
+                    style={{ color: "white", fontSize: "1.5em" }}
+                  />
+                </button>
+              ) : (
+                <button
+                  onClick={() => handleClick()}
+                  className="btn btn-success btn-circle shadow-md hover:shadow-lg"
+                >
+                  <HiHeart style={{ color: "white", fontSize: "1.5em" }} />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CarouselCard;
--- adopt-a-pal/src/routes/browse/browse.jsx
+++ adopt-a-pal/src/routes/browse/browse.jsx
@@ -1,14 +1,73 @@
-import { React, useState } from "react";
-import NavBar from "../../components/navbar";
-import Footer from "../../components/footer";
+import { React, useEffect, useState } from "react";
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import CarouselCard from '../../components/CarouselCard';
 
 function Browse(props) {
+  // const [animals, setAnimals] = useState("");
+
+  // TO DO: GET DATA FROM API
+  const animals = [
+    {
+      added: "2023-05-08 17:30:22.420313+00:00",
+      avatars: [
+        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-721",
+        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-4382",
+        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-3876",
+      ],
+      availability: "Available",
+      species: "Cat",
+      breed: "Medium Hair",
+      dispositions: [
+        "Good with other animals",
+        "Good with children",
+        "Animal must be leashed at all times",
+      ],
+      name: "Buttterscotch",
+      id: 5143677177430016,
+    },
+    {
+      added: "2023-04-29 17:30:35.750316+00:00",
+      avatars: [
+        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-5528",
+        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-2300",
+        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-8854",
+      ],
+      availability: "Available",
+      species: "Dog",
+      breed: "Cattle Dog",
+      dispositions: [
+        "Good with other animals",
+        "Animal must be leashed at all times",
+      ],
+      name: "Clair",
+      id: 5168126949851136,
+    },
+  ];
 
   return (
     <>
-      <NavBar currentPage={ "browse" } />
-      <Footer />
+      <NavBar currentPage={"browse"} />
+      <div className="w-[1280px] flex flex-col mt-36 mb-10 mx-auto justify-center ">
+        {/* PAGE TITLE */}
+        <div className="mb-8 text-start text-2xl font-bold text-brown">
+          Browse Pets
+        </div>
 
+
+
+        {/* PAGE CONTENT */}
+        <div className="carousel carousel-center w-[50em] mx-auto space-x-8 rounded-box">
+          {/* Creates CarouselCard for each item in list of animal instances */}
+          {animals?.map((animal, i) => (
+            <div className="carousel-item">
+              <CarouselCard animal={animal} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <Footer />
     </>
   );
 }
