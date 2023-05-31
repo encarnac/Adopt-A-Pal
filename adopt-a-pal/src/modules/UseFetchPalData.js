dcfb35401bf93086b1eab3672c28c1316b670eea
Noah Calhoun <w.noah.calhoun@gmail.com>
2023-05-31 13:01:22 -0500

    cards dynamically update for dashboard

--- adopt-a-pal/src/modules/UseFetchPalData.js
+++ adopt-a-pal/src/modules/UseFetchPalData.js
@@ -1,29 +1,62 @@
-import React, { useState, useEffect } from 'react';
-import UseGetPalById from './UseGetPalById';
+import { useState, useEffect } from "react";
 
-function UseFetchPalData(pals) {
-    const [palDataList, setPalDataList] = useState([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-  
-    useEffect(() => {
-      const fetchData = async () => {
+const useFetchPalData = (userData, token) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [palDataList, setPalDataList] = useState([]);
+
+  useEffect(() => {
+    const fetchPalData = async () => {
+      if (userData && userData.pals && userData.pals.length > 0) {
         try {
-          const promises = pals.map((palId) => UseGetPalById(palId));
-          const palDataList = await Promise.all(promises);
-          setPalDataList(palDataList);
+          const palDataPromises = userData.pals.map(async (palId) => {
+            const response = await fetch(`/api/animals/${palId}`, {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            });
+
+            if (!response.ok) {
+              throw new Error("Failed to fetch data");
+            }
+
+            const jsonData = await response.json();
+
+            const {
+              name,
+              added,
+              availability,
+              avatars,
+              breed,
+              dispositions,
+            } = jsonData;
+
+            const filteredData = {
+              name,
+              added,
+              availability,
+              avatars,
+              breed,
+              dispositions,
+            };
+
+            return filteredData;
+          });
+
+          const palData = await Promise.all(palDataPromises);
+          setPalDataList(palData);
           setLoading(false);
         } catch (error) {
-          console.error('Error fetching pal data:', error);
           setError(error.message);
           setLoading(false);
         }
-      };
-  
-      fetchData();
-    }, [pals]);
-  
-    return { loading, palDataList, error };
-}
+      }
+    };
 
-export default UseFetchPalData;
+    fetchPalData();
+  }, [userData, token]);
+
+  return [palDataList, loading, error];
+};
+
+export default useFetchPalData;
--- adopt-a-pal/src/routes/dashboard/dashboard.jsx
+++ adopt-a-pal/src/routes/dashboard/dashboard.jsx
@@ -9,50 +9,27 @@ import Footer from "../../components/footer";
 
 
 function Dashboard(props) {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   const decoded = jwtDecode(token);
   const uid = decoded.id;
   const userUrl = `/api/users/${uid}`;
   const userData = UseUserPals(userUrl);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  const [palDataList, setPalDataList] = useState([]);
-  console.log("user data:", userData);
 
-  useEffect(() => {
-    if (userData && userData.pals) {
-      const fetchPalData = async () => {
-        try {
-          const pallist = ["5700433016258560", "5731076903272448"];
-          const promises = pallist.map((palId) =>
-            UseGetPalById(palId)
-          );
-          const palDataList = await Promise.all(promises);
-          setPalDataList(palDataList);
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching pal data:', error);
-          setError(error.message);
-          setLoading(false);
-        }
-      };
-
-      fetchPalData();
-    }
-  }, [userData]);
+  const [palDataList, loading, error] = UseFetchPalData(userData, token);
 
   if (loading) {
     return <div>Loading...</div>;
   }
+
   if (error) {
     return <div>Error: {error}</div>;
   }
 
   return (
     <>
-      {/* <NavBar currentPage="dashboard" /> */}
-      {/* <Matches palDataList={palDataList} /> */}
-      {/* <Footer /> */}
+      <NavBar currentPage="dashboard" />
+      <Matches palDataList={palDataList} />
+      <Footer />
     </>
   );
 }
@@ -72,47 +49,73 @@ export default Dashboard;
 //   const [palDataList, setPalDataList] = useState([]);
 //   console.log("user data:", userData);
 
-
-
-//   // const { loading, palDataList, error } = UseFetchPalData(userData?.pals || []);
 //   useEffect(() => {
-//     if (userData && userData.pals) {
-//       const fetchPalData = async () => {
-//         try 
-//         {
-//           const promises = userData.pals.map((palId) =>
-//             UseGetPalById(palId)
-//           );
-//           const palDataList = await Promise.all(promises);
-//           setPalDataList(palDataList);
+//     const fetchPalData = async () => {
+//       if (userData && userData.pals && userData.pals.length > 0) {
+//         try {
+//           const palDataPromises = userData.pals.map(async (palId) => {
+//             const response = await fetch(`/api/animals/${palId}`, {
+//               headers: {
+//                 Authorization: `Bearer ${token}`,
+//               },
+//             });
+
+//             if (!response.ok) {
+//               throw new Error("Failed to fetch data");
+//             }
+
+//             const jsonData = await response.json();
+
+//             const {
+//               name,
+//               added,
+//               availability,
+//               avatars,
+//               breed,
+//               dispositions,
+//             } = jsonData;
+
+//             const filteredData = {
+//               name,
+//               added,
+//               availability,
+//               avatars,
+//               breed,
+//               dispositions,
+//             };
+
+//             return filteredData;
+//           });
+
+//           const palData = await Promise.all(palDataPromises);
+//           setPalDataList(palData);
 //           setLoading(false);
-//         } 
-//         catch (error) 
-//         {
-//           console.error('Error fetching pal data:', error);
+//         } catch (error) {
 //           setError(error.message);
 //           setLoading(false);
 //         }
-//       };
-//       fetchPalData();
-//     }
-//   }, [userData]);
+//       }
+//     };
+
+//     fetchPalData();
+//   }, [userData, token]);
 
 //   if (loading) {
 //     return <div>Loading...</div>;
 //   }
+
 //   if (error) {
 //     return <div>Error: {error}</div>;
 //   }
 
-
 //   return (
 //     <>
-//       {/* <NavBar currentPage="dashboard" /> */}
-//       {/* <Matches palDataList={palDataList} /> */}
-//       {/* <Footer /> */}
+//       <NavBar currentPage="dashboard" />
+//       <Matches palDataList={palDataList} />
+//       <Footer />
 //     </>
 //   );
 // }
 
 // export default Dashboard;
+
