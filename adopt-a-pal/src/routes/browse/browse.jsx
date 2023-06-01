ae849789a3efa084504cadbefd8313d4f990b12f
Colene Encarnado <encarnac@oregonstate.edu>
2023-06-01 15:21:08 -0700

    dynamically displays all animals from db in Browse

--- adopt-a-pal/src/routes/browse/browse.jsx
+++ adopt-a-pal/src/routes/browse/browse.jsx
@@ -1,50 +1,33 @@
 import { React, useEffect, useState } from "react";
 import NavBar from "../../components/navbar";
-// import NavBar from "../../components/NavBar";
 import Footer from "../../components/footer";
 import CarouselCard from '../../components/CarouselCard';
 
 function Browse(props) {
-  // const [animals, setAnimals] = useState("");
+  const animalUrl = "/api/animals";
+  const [ animals, setAnimals ] = useState(null);
+  const [error, setError] = useState(null);
 
-  // TO DO: GET DATA FROM API
-  const animals = [
-    {
-      added: "2023-05-08 17:30:22.420313+00:00",
-      avatars: [
-        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-721",
-        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-4382",
-        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-3876",
-      ],
-      availability: "Available",
-      species: "Cat",
-      breed: "Medium Hair",
-      dispositions: [
-        "Good with other animals",
-        "Good with children",
-        "Animal must be leashed at all times",
-      ],
-      name: "Buttterscotch",
-      id: 5143677177430016,
-    },
-    {
-      added: "2023-04-29 17:30:35.750316+00:00",
-      avatars: [
-        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-5528",
-        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-2300",
-        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-8854",
-      ],
-      availability: "Available",
-      species: "Dog",
-      breed: "Cattle Dog",
-      dispositions: [
-        "Good with other animals",
-        "Animal must be leashed at all times",
-      ],
-      name: "Clair",
-      id: 5168126949851136,
-    },
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch( animalUrl ); 
+
+        if (!response.ok) {
+          throw new Error('Failed to fetch data');
+        }
+
+        const animalData = await response.json();
+
+        console.log(animalData);
+        setAnimals(animalData);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, [animalUrl]);
+
 
   return (
     <>
@@ -54,6 +37,8 @@ function Browse(props) {
         <div className="mb-8 text-start text-2xl font-bold text-brown">
           Browse Pets
         </div>
+
+
 
 
 
