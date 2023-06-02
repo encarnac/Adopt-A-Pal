88addcbcd4ea650231bac4267d7879e26f036857
Colene Encarnado <encarnac@oregonstate.edu>
2023-06-02 10:43:06 -0700

    Changed LIKE button on click

--- adopt-a-pal/src/components/CarouselCard.jsx
+++ adopt-a-pal/src/components/CarouselCard.jsx
@@ -77,8 +77,21 @@ function CarouselCard({ animal, userID }) {
             <div className="absolute bottom-4 right-4">
               {/* Opt 1: Like Button for "browse" cardType */}
               {loading ? (
-                <button className="btn btn-success btn-circle shadow-md hover:shadow-lg">
-                  <span className="loading loading-spinner loading-sm"></span>
+                <button className="btn btn-success btn-circle shadow-md">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="#FFF1EE"
+                    viewBox="0 0 24 24"
+                    stroke="#FFF1EE"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                    />
+                  </svg>
                 </button>
               ) : (
                 <button
@@ -88,7 +101,7 @@ function CarouselCard({ animal, userID }) {
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-6 w-6"
-                    fill="#none"
+                    fill="none"
                     viewBox="0 0 24 24"
                     stroke="#FFF1EE"
                   >
