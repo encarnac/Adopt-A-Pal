89bf420548fbb795fa5f7e08bd6d5930dd8986f7
Colene Encarnado <encarnac@oregonstate.edu>
2023-06-01 17:23:51 -0700

    Added filter form inputs (not working yet)

--- adopt-a-pal/src/routes/browse/browse.jsx
+++ adopt-a-pal/src/routes/browse/browse.jsx
@@ -1,10 +1,15 @@
 import { React, useEffect, useState } from "react";
 import NavBar from "../../components/navbar";
 import Footer from "../../components/footer";
+import FilterBar from "./filterbar";
 import CarouselCard from '../../components/CarouselCard';
 
 function Browse(props) {
-  const animalUrl = "/api/animals";
+  const [animalUrl, setAnimalUrl] = useState("/api/animals");
+  const handleAnimalUrl = (e) => {
+    setAnimalUrl(e);
+  }
+
   const [ animals, setAnimals ] = useState(null);
   const [error, setError] = useState(null);
 
@@ -19,7 +24,6 @@ function Browse(props) {
 
         const animalData = await response.json();
 
-        console.log(animalData);
         setAnimals(animalData);
       } catch (error) {
         setError(error.message);
@@ -28,6 +32,43 @@ function Browse(props) {
     fetchData();
   }, [animalUrl]);
 
+//  const animals = [
+//    {
+//      added: "2023-05-08 17:30:22.420313+00:00",
+//      avatars: [
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-721",
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-4382",
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Buttterscotch2673-3876",
+//      ],
+//      availability: "Available",
+//      species: "Cat",
+//      breed: "Medium Hair",
+//      dispositions: [
+//        "Good with other animals",
+//        "Good with children",
+//        "Animal must be leashed at all times",
+//      ],
+//      name: "Buttterscotch",
+//      id: 5143677177430016,
+//    },
+//    {
+//      added: "2023-04-29 17:30:35.750316+00:00",
+//      avatars: [
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-5528",
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-2300",
+//        "https://storage.googleapis.com/adopt-a-pal-pics/Clair8747-8854",
+//      ],
+//      availability: "Available",
+//      species: "Dog",
+//      breed: "Cattle Dog",
+//      dispositions: [
+//        "Good with other animals",
+//        "Animal must be leashed at all times",
+//      ],
+//      name: "Clair",
+//      id: 5168126949851136,
+//    },
+//  ];
 
   return (
     <>
@@ -38,12 +79,11 @@ function Browse(props) {
           Browse Pets
         </div>
 
-
-
-
+        {/* FILTER AND SEARCH BAR*/}
+        <FilterBar handleAnimalUrl={handleAnimalUrl} />
 
         {/* PAGE CONTENT */}
-        <div className="carousel carousel-center w-[50em] mx-auto space-x-8 rounded-box">
+        <div className="carousel carousel-center w-[50em] mx-auto mt-12 mb-28 space-x-8 rounded-box">
           {/* Creates CarouselCard for each item in list of animal instances */}
           {animals?.map((animal, i) => (
             <div className="carousel-item">
---
+++ adopt-a-pal/src/routes/browse/filterbar.jsx
@@ -0,0 +1,76 @@
+import { React, useEffect, useState } from "react";
+
+function FilterBar( {handleAnimalUrl} ) {
+  // Changes the search paramters
+  // const handleAnimalUrl = handleAnimalUrlprops.handleAnimalUrl;
+
+  // Search Query Paramaters
+  const [species, setSpecies] = useState(null);
+  const [breed, setBreed] = useState(null);
+  const [dispositionAnimals, setDispositionAnimals] = useState(null);
+  const [dispositionChildren, setDispositionChildren] = useState(null);
+  const [dateRange, setDateRange] = useState(null);
+
+  const searchAnimals = () => {
+    const urlQuery = "";
+  }
+
+  return (
+    <>
+      <form>
+        <div className="join justify-start flex flex-row space-x-2 mb-8">
+          {/* DATE POSTED - DROP DOWN SELECT */}
+          <select className="select bg-white border-primary opacity-50 text-brown join-item">
+            <option disabled selected>
+              Date Posted
+            </option>
+            <option>Any Date</option>
+            <option>&lt; 1 week</option>
+            <option>&lt; 1 month</option>
+            <option>&lt; 6 months</option>
+          </select>
+
+          {/* SPECIES - DROP DOWN SELECTES */}
+          <select className="select bg-white border-primary opacity-50 text-brown join-item">
+            <option disabled selected>
+              Species
+            </option>
+            <option>All Species</option>
+            <option>Cat</option>
+            <option>Dog</option>
+            <option>Other</option>
+          </select>
+
+          {/* DISPOSITIONS - BUTTONS  */}
+          <button className="btn bg-white border-primary opacity-50 text-brown normal-case join-item">
+            Kid Friendly
+          </button>
+          <button className="btn bg-white border-primary opacity-50 text-brown normal-case join-item">
+            Animal Friendly
+          </button>
+
+          {/* BREED - TEXT INPUT */}
+          <div>
+            <div>
+              <input
+                className="input input-bordered border-primary bg-white opacity-50 text-brown text-sm join-item "
+                placeholder="Breed"
+                onChange={(e) => setBreed(e.target.value)}
+              />
+            </div>
+          </div>
+
+          {/* SUBMIT BUTTON */}
+          <button
+            type="submit"
+            className="btn btn-primary text-white join-item"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+
+export default FilterBar;
